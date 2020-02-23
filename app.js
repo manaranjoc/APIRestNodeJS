055f7f5ed258eca0be1ee24fccdff3f972c191b6
Miguel Angel <angel.naranjo@udea.edu.co>
2020-02-23 15:55:47 -0500

    Add error handling, log and bodyParsing

--- app.js
+++ app.js
@@ -3,12 +3,34 @@
 const express = require('express');
 
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const productionRoutes = require('./api/routes/production');
 const workerRoutes = require('./api/routes/worker');
 const emeraldRoutes = require('./api/routes/emeralds');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/production', productionRoutes);
 app.use('/worker', workerRoutes);
 app.use('/emerald', emeraldRoutes);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
